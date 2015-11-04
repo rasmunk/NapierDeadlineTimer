2ceb49e1dc2ac5c1a100d8dee7edf918b2d60ccd
Sam Serrels <doogly1@gmail.com>
2015-11-04 13:03:56 +0000

    Initial Commit

---
+++ timer.js
@@ -0,0 +1,94 @@
+
+//--- configs -------------------------
+var ScrollSpeed = 0.022;
+var ScrollWaitTime = 10000;
+
+//-------------------------------------
+var mainDiv = $("#maindiv");
+var leftDiv = $("#leftdiv");
+var rightDiv = $("#rightdiv");
+var ScrollLimit = document.body.offsetHeight;
+
+//Sort the Tiem array by soonest
+function SortTime(a, b) {
+	var ad = a.date;
+	var bd = b.date;
+	return ((ad < bd) ? -1 : ((ad > bd) ? 1 : 0));
+}
+times.sort(SortTime);
+
+//RightDiv scrolling funcs
+function ScrollUp() {
+	rightDiv.delay(ScrollWaitTime).animate({ scrollTop: 0 }, ScrollLimit / ScrollSpeed, "linear", ScrollDown);
+}
+function ScrollDown() {
+	rightDiv.delay(ScrollWaitTime).animate({ scrollTop: ScrollLimit }, ScrollLimit / ScrollSpeed, "linear", ScrollUp);
+}
+
+//Only start the scrolling functions if there is enough times to scroll.
+if (times.length > 9) {
+	ScrollUp();
+}
+
+//Left div times
+for (var i = 0; i < times.length && i < 5; i++) {
+	var t = times[i];
+	var row = $('<div />', {
+        "class": 'row timerRow',
+		id: t.name + "clockrow"
+	});
+	var nameDiv = $('<div />', {
+        "class": 'col-sm-2 timerName',
+        html: t.name+"<br><span>"+t.date.toGMTString().slice(0,-7)+"</span>"
+	});
+	var clockcontainer = $('<div />', {
+        "class": 'col-sm-10 timer'
+	});
+	var clockDiv = $('<div />', {
+        id: t.name + "clock"
+	});
+	
+	//create clock
+	var clock = clockDiv.FlipClock({ clockFace: 'DailyCounter' });
+	clock.setTime((t.date - new Date()) * 0.001);
+	clock.setCountdown(true);
+	
+	//add elements to page
+	row.append(nameDiv);
+	clockcontainer.append(clockDiv);
+	row.append(clockcontainer);
+	leftDiv.append(row);
+}
+
+//Right div times
+for (var i = 5; i < times.length; i++) {
+	var t = times[i];
+	var row = $('<div />', {
+        "class": 'row timerRow',
+		id: t.name + "clockrow"
+	});
+	var nameDiv = $('<div />', {
+        "class": 'col-sm-3 timerName timerNameRight',
+        html: t.name+"<br><span>"+t.date.toGMTString().slice(0,-7)+"</span>"
+	});
+	var clockcontainer = $('<div />', {
+        "class": 'col-sm-9 timer'
+	});
+	var clockDiv = $('<div />', {
+        id: t.name + "clock"
+	});
+	var clock = clockDiv.FlipClock({ clockFace: 'DailyCounter2' });
+	clock.setTime((t.date - new Date()) * 0.001);
+	clock.setCountdown(true);
+	row.append(nameDiv);
+	clockcontainer.append(clockDiv);
+	row.append(clockcontainer);
+	rightDiv.append(row);
+
+}
+//add a spacer to the right div, so the bottomest time get's pushed up slightly.
+rightDiv.append($('<div />', { "class": 'spacer', text: "" }));
+//figure out the scroll limits
+rightDiv.scrollTop(999999);
+ScrollLimit = rightDiv.scrollTop();
+rightDiv.scrollTop(0);
---
+++ times.js
@@ -0,0 +1,40 @@
+var times = [
+	{ name: "Lab Close", date: getLabClose()  },
+	{ name: "Fallout 4", date: new Date("10/nov/15 0:01 AM") },
+	{ name: "SAAS Day", date: getSaas() },
+	{ name: "Xmas", date: new Date("25/Dec/2015") },
+	{ name: "C&PS CW1", date: new Date("06/nov/2015 11:59 PM") },
+	{ name: "C&PS CW2", date: new Date("04/dec/2015 5:00 PM") },
+	{ name: "SE Exam", date: new Date("07/dec/2015 9:30 AM") },
+	{ name: "A&DS Exam", date: new Date("08/dec/2015 9:30 AM") },
+	{ name: "DBS Exam", date: new Date("09/dec/2015 9:30 AM") },
+	{ name: "C&PS1 Exam", date: new Date("08/dec/2015 1:30 PM") },
+	{ name: "Thanksgiving", date: new Date("26/nov/2015") },
+	{ name: "New Years", date: new Date("01/jan/2016") },
+	{ name: "A&DS CW", date: new Date("20/nov/2015 4:00 PM") },
+	{ name: "SE CW2", date: new Date("27/nov/2015 4:00 PM") },
+	{ name: "SD3 CW", date: new Date("02/dec/2015 4:00 PM") },
+	{ name: "SE CW3", date: new Date("04/dec/2015 4:00 PM") },
+
+]
+
+function getSaas() {
+	var dd = new Date();
+	if (dd.getDay() < 7) {
+		dd.setDate(7)
+	} else {
+		dd.setDate(7)
+		dd.setMonth(dd.getMonth + 1);
+	};
+	return new Date(dd.getFullYear(), dd.getMonth(), dd.getDate());
+}
+
+function getLabClose() {
+	var dd = new Date();
+	var isWeekend = (dd.getDay() == 6) || (dd.getDay() == 0);
+	dd.setHours(isWeekend ? 17 : 21);
+	dd.setMilliseconds(0);
+	dd.setMinutes(0);
+	dd.setSeconds(0);
+	return dd;
+};
