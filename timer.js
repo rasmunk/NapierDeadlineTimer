dd8d8b1343fc5e825aadf9d86fcad4d277ce2b64
40082132 <40082132@ME0B056-015029.c27-merch.napier.ac.uk>
2016-02-29 12:19:41 +0000

    Illuminati confirmed

--- timer.js
+++ timer.js
@@ -4,7 +4,7 @@ var ScrollWaitTime = 10000;
 
 //-------------------------------------
 var clocks = [];
-var snoop, doot, quack, rimshot, laughtrack, seinfield, sadtrumbone, airhorn, johncena, nemesis, victory;
+var snoop, doot, quack, rimshot, laughtrack, seinfield, sadtrumbone, airhorn, johncena, nemesis, victory, illuminati;
 var leftDiv, rightDiv;
 var ScrollLimit;
 var b56cal;
@@ -34,6 +34,7 @@ $(document).ready(function () {
   johncena = new Audio('http://www.myinstants.com/media/sounds/and-his-name-is-john-cena-1_6.mp3');
   nemesis = new Audio('http://www.myinstants.com/media/sounds/tf_nemesis.mp3');
   victory = new Audio('http://www.myinstants.com/media/sounds/victoryff.swf.mp3');
+  illuminati = new Audio('https://www.youtube.com/watch?v=fxfEg54cCKo');
   b56cal = $.parseIcs('calendar.ics');
   //console.log(b56cal);
   
@@ -188,6 +189,10 @@ $("body").keypress(function (event) {
 	case 118:
 	  victory.play();
 	  break;
+	case 105:
+	  illuminati.play();
+	  break;
+	 
 	
   }
 
